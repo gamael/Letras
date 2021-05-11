99a505710a1d34fd020dfef5d6521a50443d4ec9
Alejandro <carlosaguirre7@hotmail.com>
2021-05-10 22:51:27 -0500

    Feat: view controlares añadidos

---
+++ Letras/LetraVC.swift
@@ -0,0 +1,30 @@
+//
+//  LetraVC.swift
+//  Letras
+//
+//  Created by Alejandro Agudelo on 17/04/21.
+//  Copyright © 2021 Alejandro Agudelo. All rights reserved.
+//
+
+import UIKit
+
+class LetraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
