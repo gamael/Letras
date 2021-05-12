bdf435d072e36aef5d13a5ff37bbfb6045981839
Alejandro <carlosaguirre7@hotmail.com>
2021-05-12 10:02:01 -0500

    Feat: core data implementado para almacenamiento de canciones

--- Letras/HistorialVC.swift
+++ Letras/HistorialVC.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import CoreData
 
 class HistorialVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    fileprivate let coreDataManager = CoreDataManager(modelName: "Model")
+    fileprivate lazy var fetchedResultsController: NSFetchedResultsController<CDCancion> = {
+        // Initialize Fetch Request
+        let fetchRequest: NSFetchRequest<CDCancion> = CDCancion.fetchRequest()
+
+        // Add Sort Descriptors
+        let sortDescriptor = NSSortDescriptor(key: "fechaCreacion", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+
+        // Initialize Fetched Results Controller
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.coreDataManager.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+
+        return fetchedResultsController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            let fetchError = error as NSError
+            print("No se pudo obtener datos")
+            print("\(fetchError), \(fetchError.localizedDescription)")
+        }
     }
-
-
 }
 
+extension HistorialVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let sections = fetchedResultsController.sections else {
+                return 0
+            }
+
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "rid", for: indexPath)
+
+        // Obtener Canción
+        let cdcancion = fetchedResultsController.object(at: indexPath)
+
+        // Configurar Celda
+        cell.textLabel?.text = cdcancion.nombre
+        cell.detailTextLabel?.text = cdcancion.artista
+
+        return cell
+    }
+}
