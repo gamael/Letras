94364db6bbe8d6c247c36d64298c739ab4b0ab2e
Alejandro <carlosaguirre7@hotmail.com>
2021-05-12 10:27:09 -0500

    Feat: fetched results controller delegate implemented

--- Letras/HistorialVC.swift
+++ Letras/HistorialVC.swift
@@ -19,11 +19,13 @@ class HistorialVC: UIViewController {
         let fetchRequest: NSFetchRequest<CDCancion> = CDCancion.fetchRequest()
 
         // Add Sort Descriptors
-        let sortDescriptor = NSSortDescriptor(key: "fechaCreacion", ascending: true)
+        let sortDescriptor = NSSortDescriptor(key: "fechaCreacion", ascending: false)
         fetchRequest.sortDescriptors = [sortDescriptor]
 
         // Initialize Fetched Results Controller
         let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.coreDataManager.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        fetchedResultsController.delegate = self
 
         return fetchedResultsController
     }()
@@ -51,15 +53,60 @@ extension HistorialVC: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "rid", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cancionCell", for: indexPath)
+        
+        configureCell(cell, at: indexPath)
 
+        return cell
+    }
+    
+    private func configureCell(_ cell: UITableViewCell, at indexPath: IndexPath) {
         // Obtener Canción
         let cdcancion = fetchedResultsController.object(at: indexPath)
 
         // Configurar Celda
         cell.textLabel?.text = cdcancion.nombre
         cell.detailTextLabel?.text = cdcancion.artista
+    }
+}
 
-        return cell
+extension HistorialVC: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch (type) {
+        case .insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break;
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break;
+        case .update:
+            if let indexPath = indexPath, let cell = tableView.cellForRow(at: indexPath) {
+                configureCell(cell, at: indexPath)
+            }
+            break;
+        case .move:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+
+            if let newIndexPath = newIndexPath {
+                tableView.insertRows(at: [newIndexPath], with: .fade)
+            }
+            break;
+        @unknown default:
+            break
+        }
     }
 }
